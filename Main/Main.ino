e0dee0879c1d7115c77ecc41afe7d9a6596ee58c
Eduardo Holzbach <eduardoholzbach@gmail.com>
2021-09-21 12:22:59 -0300

    1.0

--- Main/Main.ino
+++ Main/Main.ino
@@ -6,15 +6,15 @@ const int  bot = 12 ;       // Pino em que o botão está ligado
 const int led = 7 ;         // Pino em que o LED está ligado
 const int buzzer = 6 ;      // Pino em que o Buzzer está ligado
 
-int TPomodoro = 0;          // Variável com Tempo de Pomodoro em minutos
+int TPomodoro = 0;          // Variável com Tempo de Pomodoro em segundos
 int TPomodoro_aux = 0;      // Auxíliar na leitura do tempo
 
 int bot_cont = 0;           // Contador para o número de prensas botão
 int bot_estado = 0;         // Estado atual do botão
 int bot_estado_ant = 0;     // Estado anterior do botão
 
-int estado = 0;
-int cont = 0;
+int estado = 0;             // Variavel de controle do codigo
+int cont = 0;               // Variavel auxiliar para contagem de tempo
 
 
 void setup() {
@@ -34,11 +34,10 @@ void loop() {
   if (bot_estado != bot_estado_ant) {
     if (bot_estado == LOW) {
       bot_cont++;
-      //Serial.println(bot_cont);
       delay(100);
     }
     bot_estado_ant = bot_estado;
-    if (bot_cont % 2 == 1) {
+    if ((bot_cont % 2 == 1) && (TPomodoro > 0)) {
       estado = 1; // Estado Ligado
     }
     else {
@@ -50,18 +49,16 @@ void loop() {
     case 0: //Estado Desligado
       digitalWrite(led, LOW);
       if (Serial.available()) {
-        TPomodoro_aux = Serial.parseInt();
+        TPomodoro_aux = Serial.parseInt(); // Tempo de Pomodoro em segundos
         if ((TPomodoro_aux != 0) && (TPomodoro != TPomodoro_aux)) {
           TPomodoro = TPomodoro_aux;
           Serial.println("Novo valor recebido!");
         }
-        //Serial.println(TPomodoro);
       }
       break;
     case 1: // Estado Ligado
       if (cont <= (TPomodoro * 100)) {
         cont++;
-        Serial.println(cont);
         delay(10);
         digitalWrite(led, HIGH);
       } else {
@@ -72,11 +69,15 @@ void loop() {
       break;
     case 2: // Tempo Acabou
       if (cont <= 5 * 100) { // 5 segundos
+        if(cont == 0){
+          Serial.println("Tempo Finalizado!");
+        }
         cont++;
         delay(10);
         digitalWrite(buzzer, HIGH);
         } else {
         cont = 0;
+        bot_cont = 0;
         digitalWrite(buzzer, LOW);
         estado = 0;
         }
